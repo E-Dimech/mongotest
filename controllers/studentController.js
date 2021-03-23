1ff4550f7ad29e81390232bcef1a1a35342c357a
Eric Dimech <ericdimech@hotmail.com>
2021-03-22 20:58:05 -0400

    hooked up mongoDB to app and used bootstrap for basic styling

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,4 +1,4 @@
-const express = required("express");
+const express = require("express");
 var router = express.Router();
 const mongoose = require("mongoose");
 const Student = mongoose.model("Student");
@@ -10,7 +10,7 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  if (req.body.__id == "") {
+  if (req.body._id == "") {
     insertRecord(req, res);
   } else {
     updateRecord(req, res);
@@ -26,6 +26,59 @@ function insertRecord(req, res) {
   student.save((err, doc) => {
     if (!err) {
       res.redirect("student/list");
+    } else {
+      console.log("Error during insert: " + err);
     }
   });
 }
+
+function updateRecord(req, res) {
+  Student.findOneAndUpdate(
+    { _id: req.body._id },
+    req.body,
+    { new: true },
+    (err, doc) => {
+      if (!err) {
+        res.redirect("student/list");
+      } else {
+        console.log("Error during update: " + err);
+      }
+    }
+  );
+}
+
+router.get("/list", (req, res) => {
+  Student.find((err, docs) => {
+    if (!err) {
+      res.render("student/list", {
+        list: docs,
+      });
+    } else {
+      console.log("Error in retrieval: " + err);
+    }
+  });
+});
+
+router.get("/:id", (req, res) => {
+  Student.findById(req.params.id, (err, doc) => {
+    if (!err) {
+      res.render("student/addOrEdit", {
+        viewTitle: "Update Student",
+        student: doc,
+      });
+      console.log(doc);
+    }
+  });
+});
+
+router.get("/delete/:id", (req, res) => {
+  Student.findByIdAndRemove(req.params.id, (err, doc) => {
+    if (!err) {
+      res.render("student/list");
+    } else {
+      console.log("Error in deletion" + err);
+    }
+  });
+});
+
+module.exports = router;
